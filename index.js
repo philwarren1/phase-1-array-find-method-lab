79b377b36957e7a736d651ee8c36f24c7b43e1eb
Philip <philwarren1@gmail.com>
2021-04-13 14:39:56 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const record = []
 
+    function superbowlWin(record){
+    const win = record.find(record => record.result === "W");
+        if (win)
+    {
+       return win.year;
+}
+    }
+//     1) returns a year the KC Chiefs won the superbowl
+//     2) returns undefined if the record has no win objects
